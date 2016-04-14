149b216f7781940d6602b52b5b91ace1ae3183a0
yourlei <yourlin127@gmail.com>
2016-04-14 07:58:35 -0700

    new version

--- app/models/article.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose'),
-		ArticleSchema = require('../schemas/article'),
-		Article = mongoose.model('Article', ArticleSchema);
-
-//　导出post　module
-module.exports = Article;
--- app/models/user.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose'),
-		UserSchema = require('../schemas/user'),
-		User = mongoose.model('User', UserSchema);
-
-//　导出post　module
-module.exports = User;
